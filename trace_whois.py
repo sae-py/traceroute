e19d5311f15147497fad55c7c168aac7c500e92e
saepy <sae.94@outlook.jp>
2018-05-24 21:02:50 +0900

    replaced spaces and tabs

--- trace_whois.py
+++ trace_whois.py
@@ -3,55 +3,59 @@ import socket
 from ipwhois import IPWhois
 from ipwhois.ipwhois import IPDefinedError
 
+import sys
+import socket
+
 def traceroute(dest_name, port, max_hops):
-    dest_addr = socket.gethostbyname(dest_name)
-    print("traceroute {0}({1})".format(dest_name, dest_addr))
+	dest_addr = socket.gethostbyname(dest_name)
+	print("traceroute {0}({1})".format(dest_name, dest_addr))
 
-    socket.setdefaulttimeout(10)
-    icmp = socket.getprotobyname("icmp")
-    udp = socket.getprotobyname("udp")
-    ttl = 1
+	socket.setdefaulttimeout(10)
+	icmp = socket.getprotobyname("icmp")
+	udp = socket.getprotobyname("udp")
+	ttl = 1
 
-    while True:
-        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-        send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
-        send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-        recv_socket.bind(("", port))
-        send_socket.sendto(bytes(512), (dest_addr, port))
+	while True:
+		recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+		send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+		send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+		recv_socket.bind(("", port))
+		send_socket.sendto(bytes(512), (dest_addr, port))
 
-        try:
-            curr_name, curr_addr = recv_socket.recvfrom(512)
-            curr_addr = curr_addr[0]
-            try:
-                curr_name = socket.gethostbyaddr(curr_addr)[0]
-            except socket.error:
-                curr_name = curr_addr
-        except socket.error:
-            pass
+		try:
+			curr_name, curr_addr = recv_socket.recvfrom(512)
+			curr_addr = curr_addr[0]
+			try:
+				curr_name = socket.gethostbyaddr(curr_addr)[0]
+			except socket.error:
+				curr_name = curr_addr
+		except socket.error:
+			pass
 
-        send_socket.close()
-        recv_socket.close()
+		send_socket.close()
+		recv_socket.close()
 
-        if curr_addr is not None:
-            print("{0} {1}({2})".format(ttl,curr_name,curr_addr))
 
-            try:
-                obj = IPWhois(curr_addr)
-            except IPDefinedError as e:
-                print("local host")
-            else:
-                who = obj.lookup()
-                nets = who['nets']
-                country = nets[0]['country']
-                company = nets[0]['description']
-                print("country：{0} company：{1}\n".format(country,company))
+		if curr_addr is not None:
+			print("{0} {1}({2})".format(ttl,curr_name,curr_addr))
 
-        else:
-            print("{} *".format(ttl))
+			try:
+				obj = IPWhois(curr_addr)
+			except IPDefinedError as e:
+				print("local host")
+			else:
+				who = obj.lookup()
+				nets = who['nets']
+				country = nets[0]['country']
+				company = nets[0]['description']
+				print("country：{0} company：{1}\n".format(country,company))
 
-        ttl += 1
-        if curr_name == dest_name or curr_addr == dest_addr or ttl > max_hops:
-            break
+		else:
+			print("{} *".format(ttl))
+
+		ttl += 1
+		if curr_name == dest_name or curr_addr == dest_addr or ttl > max_hops:
+			break
 
 if __name__ == "__main__":
 	port = 33434
